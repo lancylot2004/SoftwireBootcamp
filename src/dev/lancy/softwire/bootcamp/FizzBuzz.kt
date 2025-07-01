1d7f1fefeb35a5b6c91c9166bd69966365f123cc
Lancelot Liu <lancelot.liu@softwire.com>
2025-07-01 13:38:20 +0100

    feat: Adds more complex rules.

--- src/dev/lancy/softwire/bootcamp/FizzBuzz.kt
+++ src/dev/lancy/softwire/bootcamp/FizzBuzz.kt
@@ -2,11 +2,31 @@ package dev.lancy.softwire.bootcamp
 
 fun main() {
     (1..100).forEach {
-        buildString {
-            if (it % 3 == 0) append("Fizz")
-            if (it % 5 == 0) append("Buzz")
+        val items = mutableListOf<String>()
 
-            if (isEmpty()) append(it)
-        }.let(::println)
+        if (it % 11 == 0) {
+            // 11 displaces all (most) other outputs.
+            items += "Bong"
+        } else {
+            if (it % 3 == 0) items += "Fizz"
+            if (it % 5 == 0) items += "Buzz"
+            if (it % 5 == 0) items += "Bang"
+        }
+
+        // "Fezz" is put just after the first item that starts with 'B'.
+        // Output regardless of above rule for 11.
+        if (it % 13 == 0) {
+            var index = items.indexOfFirst { item -> item.startsWith('B') }
+            if (index == -1) index = items.size
+
+            items.add(index + 1, "Fezz")
+        }
+
+        if (it % 17 == 0) items.reverse()
+
+        items
+            .joinToString(separator = "")
+            .ifEmpty { it }
+            .let(::println)
     }
 }
