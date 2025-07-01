325eddcdc859b3205435d164d0d279a34994b694
Lancelot Liu <lancelot.liu@softwire.com>
2025-07-01 14:04:31 +0100

    fix: Comment corrections.

--- src/dev/lancy/softwire/bootcamp/FizzBuzz.kt
+++ src/dev/lancy/softwire/bootcamp/FizzBuzz.kt
@@ -18,8 +18,8 @@ fun main() {
             if (it % 7 == 0) items += "Bang"
         }
 
-        // "Fezz" is put just after the first item that starts with 'B'.
-        // Output regardless of above rule for 11.
+        // "Fezz" is put just before the first item that starts with 'B', and
+        // is output regardless of above rule for 11.
         if (it % 13 == 0) {
             var index = items.indexOfFirst { item -> item.startsWith('B') }
             if (index == -1) index = items.size
