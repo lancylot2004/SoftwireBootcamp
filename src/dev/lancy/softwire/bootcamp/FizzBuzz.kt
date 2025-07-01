62eb190ff54907c7a725ddfb7a775a363acb2354
Lancelot Liu <lancelot.liu@softwire.com>
2025-07-01 13:50:19 +0100

    fix: Corrects division logic and indexing.

--- src/dev/lancy/softwire/bootcamp/FizzBuzz.kt
+++ src/dev/lancy/softwire/bootcamp/FizzBuzz.kt
@@ -10,7 +10,7 @@ fun main() {
         } else {
             if (it % 3 == 0) items += "Fizz"
             if (it % 5 == 0) items += "Buzz"
-            if (it % 5 == 0) items += "Bang"
+            if (it % 7 == 0) items += "Bang"
         }
 
         // "Fezz" is put just after the first item that starts with 'B'.
@@ -19,7 +19,7 @@ fun main() {
             var index = items.indexOfFirst { item -> item.startsWith('B') }
             if (index == -1) index = items.size
 
-            items.add(index + 1, "Fezz")
+            items.add(index, "Fezz")
         }
 
         if (it % 17 == 0) items.reverse()
