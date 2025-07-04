2aeb84f113ccd3a758670a73de4a6373694a6157
Lancelot Liu <lancelot.liu@softwire.com>
2025-07-04 11:37:33 +0100

    feat: Abstracts away rule definition and application.

--- app/src/main/kotlin/dev/lancy/softwire/gildedrose/GildedRose.kt
+++ app/src/main/kotlin/dev/lancy/softwire/gildedrose/GildedRose.kt
@@ -4,10 +4,14 @@ class GildedRose(
     val items: List<Item>,
 ) {
     companion object {
-        /// Increment by which to change quality normally.
+        /**
+         * Increment by which to change quality normally.
+         */
         const val INCREMENT = 1
 
-        /// Maximum quality an item can ever have, except for "Sulfuras, Hand of Ragnaros".
+        /**
+         * Maximum quality an item can ever have, except for "Sulfuras, Hand of Ragnaros".
+         */
         const val MAX_QUALITY = 50
     }
 
@@ -27,36 +31,27 @@ class GildedRose(
         // This check is here because we are not allowed to modify [Item].
         require(item.quality >= 0) { "Quality can never be negative!" }
 
-        // Handle quality changes, **before** decrementing [sellIn].
-        when (item.name) {
+        // Ordering matters! For example, decrementing [sellIn] must happen **after** all quality
+        // updates are performed.
+        val rules = when (item.name) {
             // Aged Brie increases in quality the older it gets.
             // Disambiguate: Increment doubles **after** [sellIn] date.
-            "Aged Brie" -> item modQual INCREMENT * if (item.sellIn <= 0) 2 else 1
+            "Aged Brie" -> listOf(GildedRule.AgedBrieQualityIncrement, GildedRule.SellInDecrement)
             // Sulfuras does not change, ever.
-            "Sulfuras, Hand of Ragnaros" -> return
+            "Sulfuras, Hand of Ragnaros" -> emptyList()
             // Backstage passes increase in quality as the concert date approaches.
-            "Backstage passes to a TAFKAL80ETC concert" -> when {
-                // Quality is zeroed *after* the concert.
-                item.sellIn <= 0 -> item.quality = 0
-                item.sellIn < 6 -> item modQual INCREMENT * 3
-                item.sellIn < 11 -> item modQual INCREMENT * 2
-                else -> item modQual INCREMENT
-            }
+            "Backstage passes to a TAFKAL80ETC concert" ->
+                listOf(GildedRule.BackstageQualityIncrement, GildedRule.SellInDecrement)
             // Kotlin has no real pattern matching... :)
-            item.name if item.name.startsWith("Conjured") ->
-                // Conjured items decrease in quality twice as fast.
-                item modQual -INCREMENT * if (item.sellIn <= 0) 4 else 2
-            else ->
-                // All other items decrease in quality.
-                item modQual -INCREMENT * if (item.sellIn <= 0) 2 else 1
+            else -> {
+                val multiplier = if (item.name.startsWith("Conjured")) 2 else 1
+                listOf(
+                    GildedRule.QualityDecrement(multiplier = multiplier),
+                    GildedRule.SellInDecrement,
+                )
+            }
         }
 
-        // Finally, decrease the [sellIn] value for the item.
-        if (item.name != "Sulfuras, Hand of Ragnaros") item.sellIn--
-    }
-
-    /// Changes the quality of the item by the specified amount, ensuring it stays positive and within bounds.
-    private infix fun Item.modQual(by: Int) {
-        this.quality = (this.quality + by).coerceIn(0..MAX_QUALITY)
+        rules.forEach { it.update(item) }
     }
 }
--- app/src/main/kotlin/dev/lancy/softwire/gildedrose/Item.kt
+++ app/src/main/kotlin/dev/lancy/softwire/gildedrose/Item.kt
@@ -1,5 +1,7 @@
 package dev.lancy.softwire.gildedrose
 
+import dev.lancy.softwire.gildedrose.GildedRose.Companion.MAX_QUALITY
+
 data class Item(
     /**
      * Name of the item.
@@ -14,3 +16,11 @@ data class Item(
      */
     var quality: Int,
 )
+
+/**
+ * Changes the quality of the item by the specified amount, ensuring it stays positive and
+ * within bounds.
+ */
+infix fun Item.modQual(by: Int) {
+    this.quality = (this.quality + by).coerceIn(0..MAX_QUALITY)
+}
